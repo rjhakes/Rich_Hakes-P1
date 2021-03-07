22f563ee57f8ca2e886590b20a7704106046bd82
Rich Hakes <rjhakes@yahoo.com>
2021-03-06 20:29:01 -0700

    logging

--- StoreApp/StoreMVC/Program.cs
+++ StoreApp/StoreMVC/Program.cs
@@ -6,6 +6,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+//https://github.com/serilog/serilog-aspnetcore#request-logging
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
 
 namespace StoreMVC
 {
@@ -13,11 +17,38 @@ namespace StoreMVC
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Mictrosoft", Serilog.Events.LogEventLevel.Information)
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                //.WriteTo.File(new CompactJsonFormatter(), "./logs/log-.json")
+                .CreateBootstrapLogger();
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+                
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Host terminated unexpectedly");
+                
+            }
+            finally
+            {
+                Log.Information("Host shut down");
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, services, configuration) => configuration
+                    .ReadFrom.Configuration(context.Configuration)
+                    .ReadFrom.Services(services)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
