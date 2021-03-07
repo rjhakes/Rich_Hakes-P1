80059d847462c88e540e792191f9413501c37ea7
Rich Hakes <rjhakes@yahoo.com>
2021-03-06 20:31:09 -0700

    using serilog

--- StoreApp/StoreMVC/Controllers/HomeController.cs
+++ StoreApp/StoreMVC/Controllers/HomeController.cs
@@ -5,22 +5,52 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using StoreMVC.Models;
+using WebErrorLogging.Utilities;
 
 namespace StoreMVC.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        readonly IDiagnosticContext _diagnosticContext;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
+        /*public HomeController(IDiagnosticContext diagnosticContext)
+        {
+            _diagnosticContext = diagnosticContext ??
+                throw new ArgumentNullException(nameof(diagnosticContext));
+        }*/
+
         public IActionResult Index()
         {
-            return View();
+            //_diagnosticContext.Set("CatalogLoadTime", 1432);
+            try
+            {
+                /*Helper.WriteInformation("Information");
+                Helper.WriteDebug(null, "Debug ");
+                Helper.WriteWarning(null, "Warning ");*/
+                //throw new NotImplementedException();
+                return View();
+            }
+            catch (Exception e)
+            {
+                Helper.WriteError(e, "Error");
+                Helper.WriteFatal(e, "Fatal");
+                Helper.WriteVerbose(e, "Verbose");
+                throw new NotImplementedException();
+                
+            }
+            finally
+            {
+                
+            }
+
         }
 
         public IActionResult Privacy()
@@ -33,5 +63,7 @@ namespace StoreMVC.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
     }
 }
